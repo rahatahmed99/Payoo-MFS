e910d54ddcbd629a2a728da09418e89b16098797
rahatahmed99 <rahathossain6251@gmail.com>
2024-09-19 18:36:40 +0600

    cash out from done

--- js/cash-out.js
+++ js/cash-out.js
@@ -1,35 +1,35 @@
-// add money to the account
+// cash out to the account
 
-// step-1 add an event handler to the add money button inside the from
+// step-1 add an event handler to the cash out  button inside the from
 
-document.getElementById('btn-add-money')
+document.getElementById('btn-cash-out')
 .addEventListener('click', function(event){
   // prevent page reload after from submit
   event.preventDefault();
 
   // step-2 get money to added the account
    
-  const addMoneyInput = document.getElementById('input-add-money').value;
-  console.log(addMoneyInput);
+  const cashOutInput = document.getElementById('input-cash-out').value;
+  console.log(cashOutInput);
 
 
   // get the pin number provide
-  const pinNumberInput = document.getElementById('pin-number-input').value;
+  const pinNumberInput = document.getElementById('cash-out-pin').value;
   console.log(pinNumberInput);
 
 
   // step-3 verify pin number
   if (pinNumberInput === '1111'){
-    console.log('adding money');
+    console.log('cash out');
 
   // step-4 get the current balance
   const balance = document.getElementById('account-balance').innerText;
   console.log(balance);
 
   // step-5 add addMoneyInput with balance
-  const addMoneyNumber = parseFloat(addMoneyInput);
+  const cashOutNumber = parseFloat(cashOutInput);
   const balanceNumber = parseFloat(balance);
-  const newBalance = addMoneyNumber + balanceNumber;
+  const newBalance =  balanceNumber - cashOutNumber ;
   console.log(newBalance);
 
   // step-6 update the balance in a Dom
