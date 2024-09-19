1aa1d282a406ee47c9cb6197336c9e7ec4b95ae8
rahatahmed99 <rahathossain6251@gmail.com>
2024-09-19 12:08:20 +0600

    add money from done

--- js/home.js
+++ js/home.js
@@ -1,1 +1,48 @@
-console.log('added')
+// add money to the account
+
+// step-1 add an event handler to the add money button inside the from
+
+document.getElementById('btn-add-money')
+.addEventListener('click', function(event){
+  // prevent page reload after from submit
+  event.preventDefault();
+
+  // step-2 get money to added the account
+   
+  const addMoneyInput = document.getElementById('input-add-money').value;
+  console.log(addMoneyInput);
+
+
+  // get the pin number provide
+  const pinNumberInput = document.getElementById('pin-number-input').value;
+  console.log(pinNumberInput);
+
+
+  // step-3 verify pin number
+  if (pinNumberInput === '1111'){
+    console.log('adding money');
+
+  // step-4 get the current balance
+  const balance = document.getElementById('account-balance').innerText;
+  console.log(balance);
+
+  // step-5 add addMoneyInput with balance
+  const addMoneyNumber = parseFloat(addMoneyInput);
+  const balanceNumber = parseFloat(balance);
+  const newBalance = addMoneyNumber + balanceNumber;
+  console.log(newBalance);
+
+  // step-6 update the balance in a Dom
+  document.getElementById('account-balance').innerText = newBalance;
+
+
+  }
+  else{
+    alert('please try again')
+  }
+
+
+
+
+
+})
